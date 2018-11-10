c1bf58ceb72d314c74dab258e4f9f55eeb5b8815
liuzixi <liu.zixi@ebiz-fooge.com>
2018-11-10 13:50:11 +0800

    first commit

---
+++ src/main/java/com/csoptt/CsopttConfigApplication.java
@@ -0,0 +1,21 @@
+package com.csoptt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 启动类
+ * 配置中心
+ * 可被注册中心发现
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class CsopttConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsopttConfigApplication.class, args);
+	}
+}
